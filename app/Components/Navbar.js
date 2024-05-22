d54bc9b8d1dfc26004330bcaa4d96f3e34cb8f1f
Sumir Vats <sumirvats@gmail.com>
2024-05-22 22:13:07 +0530

    Home page basic structure completed

---
+++ app/Components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import styles from '../nav.module.css';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className={styles.ul}>
+        <li>
+          <Link href=''>About</Link>
+        </li>
+        <li>
+          <Link href=''>Contact</Link>
+        </li>
+        <li>
+          <Link href=''>Gallery</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,30 @@
+import Image from 'next/image';
+import profile from './assets/sumir.png';
+import Navbar from './Components/Navbar';
+
 export default function Home() {
   return (
     <main className='main-content'>
-      <h1>Heading</h1>
+      <div className='image'>
+        <Image
+          src={profile}
+          alt='Picture of the author'
+          className='picture'
+          width={500}
+          height={500}
+          blurDataURL='data:...'
+          placeholder='blur'
+        />
+      </div>
+
+      <div className='container'>
+        <Navbar />
+        <h1 className='heading'>Sumir Vats</h1>
+        <p className='content'>
+          Computer Engineering Undergraduate | Open Source Enthusiast | Web and
+          App Developer | Music and Singing
+        </p>
+      </div>
     </main>
   );
 }
